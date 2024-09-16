e1c2cbcde1135bf1e60e5240ac583aeea2a63f89
kc-claas <keithclaas@gmail.com>
2024-09-16 17:23:54 -0500

    middleware file created

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import authRoutes from './auth-routes'
 import apiRoutes from './api/index.js'
+import authentication from '../middleware/auth.js'
 const router = Router();
 
 // create routes for authentication and for api calls
 
 router.use('/auth', authRoutes);
-router.use('/api', apiRoutes)
+router.use('/api', authentication, apiRoutes)
 
 export default router;
