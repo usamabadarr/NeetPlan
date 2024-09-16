9a5651d9dd816ec5ce7ff61ecb47ee0bfd8aa7b1
kc-claas <keithclaas@gmail.com>
2024-09-16 17:41:39 -0500

    added authenitcation middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,3 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
 
+interface JwtPayload {
+    email: string;
+    location: string;
+}
+
+const authentication = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1]
+
+        const secretKey = process.env.JWT_SECRET_KEY || ''
+
+        jwt.verify(token, secretKey, (err, user) => {
+            if (err) {return res.sendStatus(403)}
+            else {
+                req.user = user as JwtPayload
+                return next()}
+            });
+    }
+    else {res.sendStatus(401)}
+}
+
+
+export default authentication
