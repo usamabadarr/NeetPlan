6af6bd4a46fdb5c8e4c139ac135db3913683f4df
SirrBarnes <torressergio1357@gmail.com>
2024-09-17 19:57:39 -0400

    finished landing page structure

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,20 +1,46 @@
-import {useEffect, useState } from 'react';
+import {useEffect, useLayoutEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import { retrieveTickets, deleteTicket } from '../api/ticketAPI';
 import ErrorPage from './ErrorPage';
+import Swimlane from '../components/Swimlane.tsx';
 import { TicketData } from '../interfaces/TicketData.tsx';
 import { ApiMessage } from '../interfaces/ApiMessage.tsx';
+import auth from '../utils/auth.ts';
 
 const LandingPage = () => {
-  const [tickets, setTickets] useState<TicketData[]>([]);
+  const [tickets, setTickets] = useState<TicketData[]>([]);
   const [error, setError] = useState(false);
   const [loginCheck, setLoginCheck] = useState(false);
   
   const checkLogin = () => {
-    i
+    if (auth.loggedIn()) {
+      setLoginCheck(true);
+    }
+  };
+
+  useLayoutEffect(() => {
+    checkLogin();
+  }, []);
+
+  if (error) {
+    return <ErrorPage />;
   }
+
+
   return (
     <>
+    {
+      !loginCheck ? (
+        <div className = 'login-notice'>
+          <h1>Daily Planner</h1>
+        </div>
+      ) : (
+        <div>
+          <h1>Congrats! You're logged in</h1>
+        </div>
+      )
+    }
       <h1>Daily Planner</h1>
     </>
   )
