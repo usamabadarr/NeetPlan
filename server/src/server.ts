6e8f5896c2156462478bc49e201163a93aca502a
kc-claas <keithclaas@gmail.com>
2024-09-23 11:34:57 -0500

    added express.static to server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,8 @@ import routes from './routes/index.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('../client/dist'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
